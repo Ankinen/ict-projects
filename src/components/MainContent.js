50fb9b5cf3fd1ec5b2add9add58377f52632d562
Annukka Kinnari <75733422+Ankinen@users.noreply.github.com>
2023-04-15 22:58:19 +0300

    first commit

---
+++ src/components/MainContent.js
@@ -0,0 +1,17 @@
+function MainContent() {
+    return (
+        <main>
+            <h1 className="title">
+                This is the title
+            </h1>
+            <div className="main-text">
+                <p>
+                    the content comes here
+                </p>
+            </div>
+        </main>
+    )
+
+};
+
+export default MainContent;
